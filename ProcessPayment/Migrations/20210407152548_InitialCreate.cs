5e39dd221830554cd1ad9a47202dbd9d33b361fa
Barun Kumar <barun.kumar2511@outlook.com>
2021-04-07 23:19:14 +0530

    test

---
+++ ProcessPayment/Migrations/20210407152548_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProcessPayment.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "paymentDetails",
+                columns: table => new
+                {
+                    PaymentProcessId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreditCardNumber = table.Column<string>(type: "nvarchar(16)", nullable: true),
+                    CardHolder = table.Column<string>(type: "nvarchar(16)", nullable: true),
+                    ExpirationDate = table.Column<string>(type: "nvarchar(5)", nullable: true),
+                    SecurityCode = table.Column<string>(type: "nvarchar(3)", nullable: true),
+                    Amount = table.Column<decimal>(type: "decimal(10,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_paymentDetails", x => x.PaymentProcessId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "paymentDetails");
+        }
+    }
+}
---
+++ ProcessPayment/Models/PaymentDetail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcessPayment.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentProcessId { get; set; }
+
+        
+        [Column(TypeName ="nvarchar(16)")]
+        public string CreditCardNumber { get; set; }
+        
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardHolder { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpirationDate { get; set; }
+
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; }
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Amount { get; set; }
+
+
+    }
+}
